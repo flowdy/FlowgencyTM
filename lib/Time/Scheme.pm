cb629e0747f12b624bfdda3485f4aa993a3f2dd4
Florian 'flowdy' Heß <flowdy@users.noreply.github.com>
2013-04-24 21:39:49 +0200

    Added Time::Scheme::from_json(), yet untested

--- lib/Time/Scheme.pm
+++ lib/Time/Scheme.pm
@@ -7,64 +7,139 @@ use Carp qw(carp croak);
 use Date::Calc;
 use Time::Line;
 
-my %schemes = ();
-
-has dbirow => (
+has _timelines => (
     is => 'ro',
-    isa => 'FlowDB::TimeScheme',
-    required => 1,
-    handles => [qw/name title/],
+    isa => 'HashRef[Time::Line]',
+    default => sub { {} },
+    handles => [ 'get' ],
 );
 
-has timeline => (
-    is => 'ro',
-    isa => 'Time::Line',
-    writer => '_set_timeline',
-    handles => [ 'respect', 'calc_slices', 'version' ],
-    init_arg => undef,
-);
-    
-has parent => (
-    is => 'ro',
-    isa => __PACKAGE__,
-    weak_ref => 1,
-);
-
-has variations => (
-    is => 'ro',
-    isa => 'ArrayRef[HashRef]',
-);
-
-has children => (
-    is => 'ro',
-    isa => 'ArrayRef[Time::Scheme]'
-);
-
-sub BUILD {
-    my ($self, $args) = @_;
-    
-    my $row = $self->dbirow;
-    my $tl = Time::Line->new(
-        fillIn => Time::Span->new(
-            description => $row->title,
-            week_pattern => $row->pattern,
-            from_date => '1.',  # no matter what
-            until_date => '1.', # no matter what
-        )
-    );
-    $self->_set_timeline($tl);
-    $self->update_variations;
+sub add_timeline {
+    my $self = shift;
+    my $args = ref $_[0] ? $_[0] : @_;
 }
 
-sub update_variations {}
+sub from_json {
 
-sub get {
-    my ($self, $id) = @_;
+    use Util::GraphChecker;
+    use JSON qw(from_json);
 
-    die 'to be implemented' if length($id);
+    my $scheme = from_json;
+    my (%tlines,$next_round_promise);
 
-    return $self;
+    my $grch = GraphChecker->new( axes => {
+        parents => sub {
+            my ($parent, $child) = @_;
+            push @{$parent->{children}}, $child;
+        },
+        children => sub {
+            my ($child, $parent) = @_;
+            push @{$child->{parents}}, $parent;
+        }
+    });
+
+    my %alias = ( from => 'from_date', until => 'until_date' );
+
+    PROP: while ( my ($key, $props) = each %$scheme ) {
+
+        my $parent = $props->{parent};
+        if ( defined($parent) && !ref($parent) ) {
+            if ( defined $tlines{$parent} ) {
+                $props->{parent} = $tlines{$parent};
+                $next_round_promise++;
+            }
+            else {
+                # dies if circular dependencies are detected
+                $grch->declare( $key => parents => $parent );
+                next PROP;
+            }
+        }
+
+        my %explicitly_varied_by;
+
+        foreach my $v ( @{ $props->{variations} } ) {
+
+            while ( my ($key, $alias) = each %alias ) {
+                $v->{$alias} = $_ if $_ = delete $v->{$key};
+            }
+
+            if ( blessed($v) ) {}
+            elsif ( $parent and my $var = $parent->get_variation($v->{ref}) ) {
+                $v = Time::Span->new( base_variation => $var, %$var, %$v );
+            }
+            elsif ( defined(my $tl = $tlines{$v->{ref}}) ) { 
+                next if $v->{ignore};
+                $next_round_promise++;
+                $v = $tl->get_section( $v->{from}, $v->{until} );
+            }
+            elsif ( $v->{week_pattern} ) {
+                $v = Time::Span->new(%$v, variation => $v);
+            }
+            else {
+                # dies if circular dependencies are detected
+                $grch->declare( $key => parents => $v->{ref} );
+                next PROP;
+            }
+
+            $explicitly_varied_by{ refaddr $v->line // $v->base_variation } = 1; 
+        }
+        
+        delete $scheme->{$key};
+    
+        my (@to_suggest, @to_impose);
+        if ( $parent ) {
+            @to_suggest = $parent->variations_to_suggest(\%explicitly_varied_by);
+            @to_impose  = $parent->variations_to_impose(\%explicitly_varied_by);
+        }
+    
+        if ( my $mode = $props->{inherited_variations_mode} ) {
+            if ( $mode eq 'optional' ) {
+                (@to_suggest, @to_impose) = ();
+            }
+            elsif ( $mode eq 'suggest' ) {
+                # append @to_impose to @to_suggest & empty the former
+                (@to_suggest, @to_impose) = (@to_suggest, @to_impose);
+            }
+            elsif ( $mode eq 'impose' ) {
+                # prepend @to_suggest to @to_impose & empty the former
+                (@to_impose, @to_suggest) = (@to_suggest, @to_impose);
+            }
+            else { die "unsupported inherited_variations_all mode: $mode!" }
+        }
+
+        my $tline = Time::Line->new(
+            fillIn => Time::Span->new(
+                week_pattern => $props->{pattern},
+                from => 1, until => 1,
+                     # 1 = first of current month
+                     # (doesn't matter, dynamically adjusted anyway)
+            ),
+        );
+
+        for my $v ( @to_suggest, @{$props->{variations}}, @to_impose ) {
+            next if !blessed($v);
+            $tline->respect($v);
+        }
+                       
+        $tlines{ $key } = $tline;
+
+    }
+    
+    if ( %$scheme ) {
+        if ( $next_round_promise ) {
+            $next_round_promise=0;
+            goto PROP;
+        }
+        else {
+            die "Timeline definitions with irresoluble dependencies: ",
+               join ", ", keys $scheme;
+        }
+    }
+
+    return $class->new( _timelines => \%tlines );
+
 }
+
 __PACKAGE__->meta->make_immutable;
 
 1;
