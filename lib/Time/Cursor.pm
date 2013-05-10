22558b3e8a7074e56309358d51f05131a30bb276
Florian 'flowdy' Heß <flowdy@users.noreply.github.com>
2013-05-10 09:34:57 +0200

    Moved Time::Slices and the code to walk through them into a closure. No need to weaken references, will prefer making Time::Slice refer to Time::Span weakly as this would be more elegant.

--- lib/Time/Cursor.pm
+++ lib/Time/Cursor.pm
@@ -4,12 +4,16 @@ use utf8;
 
 package Time::Cursor;
 use Carp qw(croak carp);
+use Scalar::Util qw(weaken);
 use Moose;
 
-has slices => (
+my $Config =;
+
+has _runner => (
     is => 'rw',
-    isa => 'ArrayRef',
-    auto_deref => 1,
+    isa => 'CodeRef',
+    lazy_build => 1,
+    init_arg => undef,
 );
 
 has version => (
@@ -50,31 +54,62 @@ sub ensure_capacity {
        if !$from->fix_order($to);
     $timeprofile->mustnt_start_later($from);
     $timeprofile->mustnt_end_sooner($to);
-    $self->slices([]);
+    $self->_clear_runner;
 }
             
 sub update {
-    my ($self, $time ) = @_;
+    my $self = shift;
 
     my $old = {};
     my $ref_version = $self->timeprofile->version;
-    if ( !@{$self->slices} or my $outdated = $self->version < $ref_version ) {
-        for ( $self->slices ) { $old = $_->calc_pos_data($time); }
-        $self->ensure_capacity if $outdated;
-        $self->slices($self->timeprofile->calc_slices($self));
+    if ( $self->version < $ref_version ) {
+        if ( $self->runner_initialized ) {
+            $old = eval { $self->runner->($time,0); } // { error => $@ };
+        }
+        $self->ensure_capacity;
         $self->version($ref_version);
     }
 
-    my %ret = map { $_ => 0 } qw(elapsed_pres remaining_pres
-                                 elapsed_abs  elapsed_abs);
-    $ret{old} = $old if $old;
+    my $data = $self->runner->(@_);
+    $data{old} = $old if $old;
 
-    $_->calc_pos_data($time,\%ret) for $self->slices;
+    return $data;
+}
 
-    return %ret, current_pos =>
-        $ret{elapsed_pres} / ($ret{elapsed_pres} + $ret{remaining_pres})
-        ;
+sub _build_runner {
+    my ($self, $block_size, $count) = @_;
 
+    my $slices = $self->timeprofile->calc_slices($self);
+
+    # Schwäche Rückreferenzen, an Anfang und Ende aber nur dann, wenn es sich
+    # um die von den jeweiligen Zeitspannen referenzierten Arrays handelt.
+    # (Dazwischen gehen wir davon aus, wir wollen ja Speicher sparen.)
+    weaken($_) for @{$slices}[ 1 .. @$slices-2 ],
+                   grep { defined && $_ == $_->span->cached_slice }
+                        @{$slices}[ 0,  -1 ]
+               ;
+
+    $block_size //= $config{block_size};
+    my ($lcnt, $rcnt) = @{ $count // [
+        $config{count_left}  // $config{count_both},
+        $config{count_right} // $config{count_both},
+    ]};
+
+    return sub {
+        my ($time) = @_;
+
+        my %ret = map { $_ => 0 } qw(elapsed_pres remaining_pres
+                                     elapsed_abs  elapsed_abs);
+        $ret{old} = $old if $old;
+
+        $_->calc_pos_data($time,\%ret) for $slices;
+
+        $ret{current_pos} = $ret{elapsed_pres}
+            / ( $ret{elapsed_pres} + $ret{remaining_pres} )
+                          ;
+
+        return \%ret;
+    }
 }
 
 sub alter_coverage {
