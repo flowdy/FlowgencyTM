0291d1af4b0b169b139b06b808bdadef2ef09ae7
Florian 'flowdy' Heß <flowdy@users.noreply.github.com>
2014-01-11 15:35:10 +0100

    changed an accessor from_date to until_date

--- lib/FlowDB/TimeLine.pm
+++ lib/FlowDB/TimeLine.pm
@@ -6,7 +6,7 @@ use Moose;
 extends 'DBIx::Class::Core';
 
 __PACKAGE__->table('timeline');
-__PACKAGE__->add_columns(qw/ task_id from_date timeprofile lock_opt /);
+__PACKAGE__->add_columns(qw/ task_id until_date timeprofile lock_opt /);
 
 1;
 
