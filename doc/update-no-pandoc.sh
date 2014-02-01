61c497a9390ee405082439eaac4ed55722d43fe4
Florian 'flowdy' Heß <flowdy@users.noreply.github.com>
2014-02-01 23:42:05 +0100

    intro04.de.txt: moved section »Kritische Bemerkungen« to the end; trivial changes

---
+++ doc/update-no-pandoc.sh
@@ -0,0 +1,15 @@
+INFILE="$1.txt"
+OUTFILE="$1.html"
+
+cat <<HEADER
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>FlowTime – Grundlagen</title>
+HEADER
+cat style.css
+echo "</head><body>"
+
+markdown $INFILE
+
+echo "</body></html>"
