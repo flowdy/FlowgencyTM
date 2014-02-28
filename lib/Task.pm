2889b29ef3116a4962ec1a273dd6a14a4c10b109
Florian 'flowdy' Heß <flowdy@users.noreply.github.com>
2014-02-28 11:36:35 +0100

    User::Tasks->add, copy, update, delete instead of _task-functions; Task->store and utility functions for substeps/step-data-logic.
    changed:   lib/Task.pm
      * sub store {}, substeps-Flattener
    
    changed:   lib/User/Tasks.pm
      * *_task routines replaced by add, copy, update and delete
      * first three now expect task name and the hash for Task::store()

--- lib/Task.pm
+++ lib/Task.pm
@@ -10,7 +10,7 @@ has _cursor => (
     required => 1,
     lazy => 1,
     builder => '_build_cursor',
-    handels => {
+    handles => {
         update_cursor => 'update',
         test_apply_time_stages => 'apply_stages'
     },
@@ -19,7 +19,7 @@ has _cursor => (
 has name => (
     is => 'ro',
     isa => 'Str',
-    default => sub { shift->dbicrow->name }
+    default => sub { shift->dbicrow->name },
     init_arg => undef,
 );
 
@@ -54,7 +54,8 @@ has ['step_retriever', 'track_finder'] => (
 
 sub _build_cursor {
     use Time::Cursor;
-    my $row = shift->dbicrow;
+    my $self = shift;
+    my $row = $self->dbicrow;
     my $cursor = Time::Cursor->new({
         timestages => $self->track_finder->($row->timestages),
     });
@@ -76,16 +77,16 @@ sub redraw_cursor_way {
         $cursor->apply_stages( @stages );
         $row->timestages(
             map { $_->{track} = $_->{track}->id }
-                $cursor->timeway_to_stage_hrefs;
+                $cursor->timeway_to_stage_hrefs
         );
     }
 
     elsif ( !@stages || ref $stages[0] eq 'ARRAY' ) {
-        @stages = $pr->(
+        my @stages = $tr->(
             $row->timestages(
                 ($_ = shift @stages ) ? @$_ : ()
             )
-        )
+        );
         $stages[0]{from_date} = ( $row->from_date );
         $cursor->change_way(@stages);
     }
@@ -97,30 +98,41 @@ sub redraw_cursor_way {
 }
 
 sub store {
-
     my $self = shift;
     my $args = @_ % 2 ? shift : { @_ };
- 
-    my $root = %$args == 1
-             ? do {
-                   my ($k) = keys %$args;
+
+    # Get name of upmost step to be stored 
+    my $root = %$args == 1    # store() callable with { $step_name => \%data }
+             ? do {           # or with { (step => $name), %further_data }
+                   my ($k) = keys %$args;   # ^ omitted? => task/main_step data
                    ($args) = values %$args;
                    $k;
                }
-             : delete $args->{step};
+             : delete $args->{step} // ''; 
 
     $args->{oldname} = $root;
 
-    my $steps = $self->_store_flatten_substeps_tree($args);
+    my $steps = delete $args->{steps} // {};
 
-    $self->result_source->storage->txn_do(
-        \&_store_write_to_db => $self, $steps, $root
+    # Make sure %$steps sub-hash has all existing steps with value {} at least
+    for my $step ( $self->steps->search( {}, { columns => [qw/name/] } ) ) {
+        $steps->{ $step->name } //= {};
+    }
+
+    $steps = [ # was hash, is now array reference
+        $self->_sequence_step_data_hrefs(
+            $root, $steps, delete $args->{substeps},
+        )
+    ];
+
+    $self->dbicrow->result_source->storage->txn_do(
+        \&_store_write_to_db => $self, $root, $args, $steps,
     );
 
     my @to_recalc;
 
     while ( my ($step, $args) = each %$steps ) {
-        next if !grep defined($_), @{$args}{qw{ done checks exp_oftime }};
+        next if !grep { defined($_) } @{$args}{qw{ done checks exp_oftime }};
         push @to_recalc, $step;
     }
 
@@ -129,7 +141,67 @@ sub store {
     };
 }
 
-sub _store_flatten_substeps_tree {
+sub _sequence_step_data_hrefs {
+    my ($parent_name, $steps_href, $top_sequence) = @_;
+
+    my %parent_of;   # %seen with parent names as values:
+                     # Used to prevent circular references that
+                     # would certainly cause endless loops
+
+    my $sequencer = sub {
+        my $sequence = shift;
+
+        my ($defined_pos, $undefined_pos) = split m{\s*;\s*}x, $order, 2;
+        my  @defined_pos                  = split m{   ,\s*}x, $defined_pos;
+        my ($order_num, @steps)           = ( undef, () );
+
+        CLUSTER:
+        for my $cl ( $undefined_pos, @defined_pos ) {
+
+            next CLUSTER if !defined $cl;
+
+            for my $step_name ( split m{ [/] }x, $cl ) {
+                my $step = $steps_href->{$step_name};
+
+                croak qq{No step "$step_name" defined} if !$step;
+                croak qq{Step already subordinated to $parent_of{$step}}
+                    if $parent_of{$step};
+
+                @{$step}{ 'oldname', 'parent',     'pos'      }
+                    = ( $step_name,  $parent_name, $order_num );
+                
+                push @steps, $step;
+   
+            }
+            
+        }
+        continue {
+            $order_num++;
+        }
+        
+        return @steps;
+
+    }; # end of $sequencer definition
+
+    my @steps = $sequencer->($top_sequence);
+    my @ordered_steps;
+
+    while ( my $step = shift @steps ) {
+
+        if ( my $substeps = delete $step->{substeps} ) {
+            $parent_name = $step->{name} // $step->{old_name};
+            unshift @steps, $sequencer->($substeps);
+        }
+
+        push @ordered_steps, $steps;
+
+    }
+
+    return @ordered_steps;
+
+}
+
+sub _DEPRECATED_store_flatten_substeps_tree {
     my ($self, $args) = @_;
 
     $args->{_level} = 0;
--- lib/User/Tasks.pm
+++ lib/User/Tasks.pm
@@ -48,9 +48,24 @@ sub _build_task_obj {
             { track => $track, until => $_->until };
         } @_ ]},
     );
-};
+}
 
-sub get_task {
+sub _next_unused_name {
+    my ($self, $prefix) = @_;
+    my $search_expr = $prefix ? { like   => "$prefix%" }
+                    :           { regexp => '^[0-9]+$' }
+                    ;
+    $name = $self->tasks->search(
+        { name => $search_expr }, { columns => ['name'] }
+    )->get_column('name')->max();
+
+    $name =~ s{ (?<!\d) (\d*) \z }{ $1+1 }exms;
+
+    return $name;
+    
+}
+
+sub get {
     my ($self, $id) = @_;
 
     return $self->cache->{$id} ||= do {
@@ -59,7 +74,74 @@ sub get_task {
     };
 }
 
-sub new_task {
+sub add {
+    my ($self, $md_href) = @_;
+    my $tasks_rs    = $self->task_rs;
+    my $name = delete $md_href->{name}
+            // $self->_next_unused_name(
+                 delete $md_href->{incr_name_prefix}
+               )
+             ;
+    
+    my $row = $tasks_rs->new({ name => $name });
+    my $task = $self->cache->{$row->name}
+             = $self->_build_task_obj($row);
+
+    $task->store($md_href);
+    
+    return $task;
+
+}
+
+sub copy {
+    my ($self, $existing_task_name, $md_href) = @_;
+
+    my $base_row = $tasks_rs->find($existing_task_name)
+        // croak "No task with name $existing_task_name found";
+
+    my $name = delete $md_href->{name}
+            // $self->_next_unused_name($existing_task_name);
+
+    return $self->cache->{$name}
+         = $base_row->result_source->storage->txn_do(sub {
+               my $t = $base_row->copy({ name => $name });
+               $t = $self->_build_task_obj($t);
+               $t->store($md_href);
+               return $t;
+         });
+}
+
+sub update {
+    my ($self, $existing_task_name, $md_href) = @_;
+    my $new_name = $md_href->{name};
+    my $task = $self->cache->{$existing_task_name}
+    $task->store($md_href);
+    if ( $new_name ) {
+        $self->cache->{$new_name} = delete $self->cache->{$existing_task_name};
+    }
+    return $task;
+}
+
+sub delete {
+    my ($self, $name) = @_;
+    if ( my $task = delete $self->cache->{$id} ) {
+        $task->dbirow->delete;
+    }
+    elsif ( my $row = $self->task_rs->find($id) ) {
+        $row->delete;
+    }
+    else {
+        carp "no task $id to delete";
+        return 0;
+    }
+    return 1;
+}
+    
+1;
+
+__END__
+
+sub new_task { # do not use
     my ($self, $id, $copy_flag) = @_;
     my $task_rs = $self->task_rs;
 
@@ -78,19 +160,3 @@ sub new_task {
 
 }
 
-sub delete_task {
-    my ($self, $id) = @_;
-    if ( my $task = delete $self->cache->{$id} ) {
-        $task->dbirow->delete;
-    }
-    elsif ( my $row = $self->task_rs->find($id) ) {
-        $row->delete;
-    }
-    else {
-        carp "no task $id to delete";
-        return 0;
-    }
-    return 1;
-}
-    
-1;
