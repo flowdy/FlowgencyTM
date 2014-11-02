cb47879a5472348e9883bb2c6799e61181213c55
Florian "flowdy" Heß <flowdy@users.noreply.github.com>
2014-11-02 15:42:11 +0100

    FTM::User: id -> user_id

--- lib/FTM/User.pm
+++ lib/FTM/User.pm
@@ -12,7 +12,7 @@ has _dbicrow => (
     is => 'ro',
     isa => 'FTM::FlowDB::User',
     required => 1,
-    handles => [qw/ id username insert update in_storage /],
+    handles => [qw/ user_id username insert update in_storage /],
     init_arg => "dbicrow",
 );
 
