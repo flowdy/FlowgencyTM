5b5e74f85f650ae7cf234ff7ccf7915e909dde33
Florian "flowdy" Heß <flowdy@users.noreply.github.com>
2015-06-07 14:59:06 +0200

    Added info page

--- lib/FlowgencyTM/Server/Info.pm
+++ lib/FlowgencyTM/Server/Info.pm
@@ -4,13 +4,14 @@ package FlowgencyTM::Server::Info;
 use FlowgencyTM;
 use Mojo::Base 'Mojolicious::Controller';
 
+my $server_started = scalar localtime(time); 
 sub basic {
     my ($self) = @_;
     $self->stash(
         version => $FlowgencyTM::VERSION,
         commit_id => qx{git rev-list -1 HEAD},
         changes => qx{git diff-index --shortstat HEAD},
-        server_started => scalar localtime(time),
+        server_started => $server_started,
     );
 }
 
