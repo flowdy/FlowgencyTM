2accf890143b0259048edf62be700f61843d4c0f
Florian "flowdy" Heß <flowdy@users.noreply.github.com>
2015-08-06 22:18:49 +0200

    schema definitions directory will keep all versions in order to enable incremental manual schema updates; script/install.sh now checks if database file exists and quits in that case

--- script/install.sh
+++ script/install.sh
@@ -6,83 +6,18 @@ export FLOWDB_SQLITE_FILE="${1:-flow.db}"
 echo -n "Initializing database file $FLOWDB_SQLITE_FILE ... "
 sqlite_cmd=$(which sqlite3)
 if [ -z "$sqlite_cmd" ]; then
-    echo 'FAILED: Cannot find and execute `sqlite3` command!'
+    echo 'FAILED: Cannot find and execute `sqlite3` command!' 1>&2
+    exit 1
 fi
 
-$sqlite_cmd $FLOWDB_SQLITE_FILE <<'END_OF_SQL' && echo OK.
-CREATE TABLE task (
-  task_id INTEGER PRIMARY KEY NOT NULL,
-  user_id ,
-  name ,
-  title  NOT NULL,
-  main_step_id ,
-  from_date  NOT NULL,
-  priority INTEGER NOT NULL,
-  open_since INTEGER,
-  archived_because ,
-  archived_ts ,
-  repeat_from ,
-  repeat_until ,
-  frequency ,
-  client ,
-  FOREIGN KEY (main_step_id) REFERENCES step(step_id),
-  FOREIGN KEY (user_id) REFERENCES user(user_id) ON DELETE CASCADE ON UPDATE CASCADE
-);
-CREATE TABLE timesegment (
-  task_id  NOT NULL,
-  until_date  NOT NULL,
-  track  NOT NULL,
-  lock_opt ,
-  FOREIGN KEY (task_id) REFERENCES task(task_id) ON DELETE CASCADE ON UPDATE CASCADE
-);
-CREATE INDEX task_idx_main_step_id ON task (main_step_id);
-CREATE INDEX task_idx_user_id ON task (user_id);
-CREATE INDEX timesegment_idx_task_id ON timesegment (task_id);
-CREATE UNIQUE INDEX user_task ON task (user_id, name);
-CREATE UNIQUE INDEX task_mainstep ON task (main_step_id);
-CREATE TABLE step (
-  step_id INTEGER PRIMARY KEY NOT NULL,
-  checks INTEGER NOT NULL DEFAULT 1,
-  done INTEGER NOT NULL DEFAULT 0,
-  description ,
-  expoftime_share INTEGER NOT NULL DEFAULT 1,
-  name  NOT NULL DEFAULT '',
-  link_id INTEGER,
-  parent_id INTEGER,
-  pos FLOAT,
-  task_id INTEGER NOT NULL,
-  FOREIGN KEY (link_id) REFERENCES step(step_id) ON DELETE CASCADE,
-  FOREIGN KEY (parent_id) REFERENCES step(step_id) ON DELETE CASCADE,
-  FOREIGN KEY (task_id) REFERENCES task(task_id) ON DELETE CASCADE ON UPDATE CASCADE
-);
-CREATE INDEX step_idx_link_id ON step (link_id);
-CREATE INDEX step_idx_parent_id ON step (parent_id);
-CREATE INDEX step_idx_task_id ON step (task_id);
-CREATE UNIQUE INDEX tree ON step (task_id, name);
-CREATE INDEX links ON step (link_id);
-CREATE TABLE "user" (
-  user_id  NOT NULL,
-  password  NOT NULL,
-  weights  NOT NULL,
-  time_model  NOT NULL,
-  priorities  NOT NULL,
-  appendix FLOAT NOT NULL DEFAULT 0.1,
-  username ,
-  email ,
-  created DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (user_id)
-);
-CREATE TABLE mailoop (
-  user_id  NOT NULL,
-  type  NOT NULL,
-  token  NOT NULL,
-  value ,
-  request_date DATETIME,
-  PRIMARY KEY (user_id),
-  FOREIGN KEY (user_id) REFERENCES user(user_id) ON DELETE CASCADE
-);
-CREATE UNIQUE INDEX token ON mailoop (token);
-END_OF_SQL
+if [ -e "$FLOWDB_SQLITE_FILE" ]; then
+    echo 'FAILED: File exists.' 1>&2
+    exit 1
+fi
+
+current_schema=$(ls -r schema/version_* | head -1)
+$sqlite_cmd $FLOWDB_SQLITE_FILE < $current_schema && echo OK. && \
+    ln -s "$(basename $current_schema)" schema/used_version
 
 USER=$(whoami)
 cat <<'END_OF_INFO'
@@ -110,7 +45,8 @@ Please follow the syntax outlined by the examples below:
 END_OF_INFO
 
 read -e -p '? Default rhythm of time model: ' -i 'Mo-So@0-23' TIME_MODEL
-perl -Ilib -MFlowgencyTM <<END_OF_PERL && echo 'Default time track changed. Now you can skip the first step listed in the Get Started screen.'
+echo 'Now, issue `script/daemon start`, then open the URL shown in your webbrowser.'
+perl -Ilib -MFlowgencyTM <<END_OF_PERL && echo 'Default time track changed here, therefore you can skip step #1 explained in the Get Started screen.' 
 my \$user = FlowgencyTM::user('$USER',1);
 \$user->insert;
 if ( length '$TIME_MODEL' && '$TIME_MODEL' ne 'Mo-So@0-23' ) {
