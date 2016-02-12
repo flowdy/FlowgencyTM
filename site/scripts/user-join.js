b3b6923c33398c684aebcc7a6c10ad2a488cd9ec
Florian "flowdy" Heß <flowdy@users.noreply.github.com>
2016-02-12 18:09:06 +0100

    Some re-work on terms of use page

--- site/scripts/user-join.js
+++ site/scripts/user-join.js
@@ -1,6 +1,6 @@
 $(function () {
-    var regform = $('#registration'), terms_accept_bitmask = [0,0,0,0,0,0,0];
-    regform.hide();
+    var regform = $('#registration'), terms_accept_bitmask = [1,1,1,1,1,1,1];
+    regform.prop('disabled', true);
     $('input[name=passw_confirm]').change(function (e) {
         var f = $(this);
         if ( f.closest("form").find("input[name=password]").val() != f.val() ) {
@@ -9,11 +9,32 @@ $(function () {
             setTimeout(function () { f.focus() }, 0);
         }
     });
-    $('ol#terms input:checkbox').each(function (i, v) {
-        $(this).click(function (e) {
-            terms_accept_bitmask[i] = this.checked ? 1 : 0;
-            var idx = parseInt(terms_accept_bitmask.join(""), 2);
-            regform[ idx == 117 || idx == 85 ? "show" : "hide" ]("fast");
+
+    $('ol#terms li').each(function (i, v) {
+        var li = $(this);
+        $('input:radio', li).click(function (e) {
+
+            terms_accept_bitmask[i] = this.value == 1 ? 2 : 0;
+            console.log( 'value(' + i + '): ' + this.value + ' | ' + terms_accept_bitmask.join(""));
+
+            var idx = parseInt(terms_accept_bitmask.join(""), 3),
+                ok = idx == 2126 || idx == 1640,
+                j, p
+                ;
+
+            // verify that no trit is left to 1 (undefined):
+            if ( ok ) for ( j = 6; j >= 0; j-- ) {
+                p = Math.pow(3, j);
+                if ( idx - p < 0 ) continue;
+                else idx -= 2 * p;
+                if ( idx < 0 ) {
+                   ok = false;
+                   break;
+                }
+            }
+
+            regform.prop('disabled', !ok );
+
         });
     });
  
