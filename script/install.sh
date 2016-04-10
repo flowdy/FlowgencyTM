f34c891f5e2877339bfcdda7b9232ab034f4ed05
Florian "flowdy" Heß <flowdy@users.noreply.github.com>
2016-04-10 14:25:53 +0200

    Reworked installation routine which, among other things, asks for a administrator password now.

--- script/install.sh
+++ script/install.sh
@@ -21,50 +21,126 @@ $sqlite_cmd $FLOWDB_SQLITE_FILE < $current_schema && echo OK. && \
 
 USER=$(whoami)
 cat <<'END_OF_INFO'
-Now, please define the standard rhythm of default time track.
-Otherwise, if you just hit ENTER, FlowgencyTM expects in urgency calculations that you work non-stop 24/7. There will soon be a convenient time model configuration in the user settings. The "How to get started" screen that already appears to a new user will recommend it as the initial step. But it is being developped, yet. What you see is a prototype only. It waits for JSON input and is not quite intended for wider use. Hence, for the time being you should define it rather now.
 
-Please follow the syntax outlined by the examples below: 
+1 of 2) DEFAULT TIME TRACK
+=========================================================================
+
+Please define the standard rhythm of default time track. Otherwise,
+if you just hit ENTER, FlowgencyTM expects in urgency calculations that
+you work non-stop 24/7. The "Get Started" screen that appears if the user
+has no tasks yet, will provide a link to a settings form which also
+includes the time model configuration so you can change it easily.
+
+Please follow the syntax outlined in the examples below:
+--------------------------------------------------------
+ 
   * Mo-Fr@9-16
     --> Traditional work day pattern, "Nine to five"
      !  Note: Omitted minute part in "until" times implies HH:59
+
   * Mo-Fr@9-16,!12
     --> same with a lunch break from 12 to 12:59
+
   * Mo-Tu,Th-Sa@...
     --> Separate also the week days with comma if needed
-     !  Note: Mo-Fr,!We@... wouldn't get parsed, exclusion is supported for the hours only.
+     !  Note: Mo-Fr,!We@... wouldn't get parsed, exclusion is supported
+        for the hours only.
+
   * Mo-We@9-12,Th-Sa@15-18,Sa@14,!18
-    --> different patterns for the halfs of a week, the specifications for Saturday will get merged.
-     !  Note: When indicating minutes prefer common parts like halfs, thirds or
-        quaters of the hour to save hardware resources. The less equal parts an hour
-        needs to be resolved into, the more efficiently the pattern can be stored
-        and processed.
+    --> different patterns for the halfs of a week, the specifications
+        for Saturday will get merged.
+     !  Note: When indicating minutes prefer common parts like halfs,
+        thirds or quaters of the hour to save hardware resources. The less
+        equal parts an hour needs to be resolved into, the more
+        efficiently the pattern can be stored, and the faster it is
+        processed, too.
+
   * 2n:Mo-Tu@9-16,We@9-12;2n+1:We@12-16,Th-Fr@9-16
     --> different patterns for alternate weeks (based on ISO week number)
 
 END_OF_INFO
 
 read -e -p '? Default rhythm of time model: ' -i 'Mo-Su@0-23' TIME_MODEL
-echo 'Now, issue `script/daemon start`, then open the URL shown in your webbrowser.'
-perl -Ilib -MFlowgencyTM <<END_OF_PERL && echo 'Default time track changed here, therefore you can skip step #1 explained in the Get Started screen.' 
+
+cat <<"EOF"
+
+
+2 of 2) PASSWORD
+========================================================================
+
+If you want to share the service with other users – beware, you are on
+your own to care for enough reliability and their privacy! – you need a
+password securing your admin account. Please set it at the prompt below
+(password is not printed on screen while typing).
+
+Do you want to make the service accessible from other devices? Then please
+adjust (later) MOJO_LISTEN in local.rc file to something different from
+IP 127.0.0.1 or localhost, according to your local network configuration. 
+
+Otherwise, just press enter so you are auto-logged in, given you use the
+same device as the server is running on.
+
+EOF
+
+TMPDIR=$(mktemp -d)
+
+NO="#"
+YES=
+while true; do
+    read -rs -e -p "? Password for admin account ($USER): " PASSWORD
+    if [ "$PASSWORD" == "" ]; then
+       IS_ADMIN=$NO
+       AUTOLOGIN=$YES
+       break
+    else
+       IS_ADMIN=$YES
+       AUTOLOGIN=$NO
+       echo
+    fi
+    read -rs -e -p "? Please confirm password by re-entering: " PASSWORD2
+    [ "$PASSWORD" == "$PASSWORD2" ] && break
+    echo
+done
+
+echo
+echo $PASSWORD > $TMPDIR/pw
+
+perl -Ilib -MFlowgencyTM <<END_OF_PERL
+
 my \$user = FlowgencyTM::user('$USER',1);
+\$0="FlowgencyTM setup script";
 \$user->insert;
 if ( length '$TIME_MODEL' && '$TIME_MODEL' ne 'Mo-Su@0-23' ) {
     \$user->update_time_model({
        default => { label => 'May mindful work clean breaks from worry', week_pattern => '$TIME_MODEL' }
     });
 }
-else { exit 1; }
+chomp(my \$pw = do {
+    open my \$fh, '<', '$TMPDIR/pw' or die;
+    unlink '$TMPDIR/pw';
+    local \$/ = undef;
+    <\$fh>;
+});
+if ( length \$pw ) {
+   \$user->salted_password(\$pw);
+   \$user->update();
+}
+else {
+   warn "Password has no length.\n"
+}
 END_OF_PERL
 
 cat > local.rc <<END_OF_CONFIG
-FLOWGENCYTM_USER=$USER
+${AUTOLOGIN}FLOWGENCYTM_USER=$USER
 FLOWDB_SQLITE_FILE=$FLOWDB_SQLITE_FILE
-FLOWGENCYTM_ADMIN=\$FLOWGENCYTM_USER # to activate new accounts
-MAX_USERS_IN_CACHE=5                 # if multi-user web access permitted
+${IS_ADMIN}FLOWGENCYTM_ADMIN=$USER # to activate new accounts
+MAX_USERS_IN_CACHE=5                  # ... if multi-user web access permitted
 MOJO_LISTEN=http://127.0.0.1:3000
 PIDFILE=/var/lock/flowgencytm.pid
 LOG=server.log
 COMMAND=morbo    # or 'server daemon', 'server prefork'
 END_OF_CONFIG
+
+echo
+echo 'Now, please issue `script/daemon start`, then load the URL that is shown.'
 
